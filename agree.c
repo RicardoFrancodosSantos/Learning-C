b23bb8b48a3f0b3826b9371b583a909ee0c40786
Ricardo Franco <ricardofrancosantos@gmail.com>
2025-02-10 09:22:04 -0300

    agree.c exercise

---
+++ agree.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+ int main (void)
+
+ {
+    char c = get_char(" do you agree?");
+      if (c == 'y')
+  {
+     printf("Agreed");
+  }
+
+  else
+  {
+   printf("Not agreed");
+  }
+
+ }
