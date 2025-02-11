81e2c07245452c38ead776b7dcdb31487fce0d2a
Ricardo Franco <ricardofrancosantos@gmail.com>
2025-02-11 07:13:11 -0300

    cat.c  exercise

--- agree.c
+++ agree.c
@@ -1,19 +1,18 @@
-// i want to make a program that get a c char value and asks to user if he agrees with the question
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
 
+void meow (void);
+
+int main (void)
 {
-    char c = get_char(" do you agree?");
-    if (c == 'y' || c == 'Y')
-    {
-        printf("Agreed\n");
-    }
+     for (int i = 0; i <3; i++)
+     {
+      meow();
+     }
+     }
 
-    else if (c == 'n' || c == 'N')
-    {
-
-        printf("Not agreed.\n");
-    }
-}
+void meow (void)
+{
+printf ("meown\n");
+     }
